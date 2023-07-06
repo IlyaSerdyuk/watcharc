69f8bc02a12d73b236e3c31ee0d7f14bdac8ef77
Ilya Serdyuk <ilya@serdyuk.pro>
2023-07-06 20:49:30 +0300

    Унифицировал файлы конфигураций PostCSS и TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const tailwindСssConfig = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -16,3 +16,5 @@ module.exports = {
   },
   plugins: [],
 };
+
+module.exports = tailwindСssConfig;
