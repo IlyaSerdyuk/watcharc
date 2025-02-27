63af39d4ca85c1fdf5c25d4f0dd551a4c108e271
Ilya Serdyuk <ilya@serdyuk.pro>
2025-02-27 23:47:36 +0300

    Улучшил отображение моделей в табличном виде

--- src/app/[lng]/(pages)/brand/[alias]/models/[viewType]/_components/ModelsTable.tsx
+++ src/app/[lng]/(pages)/brand/[alias]/models/[viewType]/_components/ModelsTable.tsx
@@ -11,66 +11,64 @@ export default function ModelsTable({
 }) {
   return (
     <div className="mt-8 flow-root">
-      <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-          <table className="min-w-full divide-y divide-gray-300">
-            <thead>
-              <tr>
-                <th
-                  scope="col"
-                  className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
-                >
-                  {t('reference')}
-                </th>
-                <th
-                  scope="col"
-                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                >
-                  {t('model-title')}
-                </th>
-                <th
-                  scope="col"
-                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                >
-                  {t('movement-type')}
-                </th>
-                <th
-                  scope="col"
-                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                >
-                  {t('gender')}
-                </th>
-                <th
-                  scope="col"
-                  className="py-3.5 pl-3 text-left text-sm font-semibold text-gray-900 pr-4 sm:pr-0"
-                >
-                  {t('skeleton')}
-                </th>
+      <div className="inline-block min-w-full py-2 align-middle">
+        <table className="min-w-full border-separate border-spacing-0">
+          <thead>
+            <tr>
+              <th
+                scope="col"
+                className="sticky top-0 z-10 border-b border-gray-300 bg-white/75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+              >
+                {t('reference')}
+              </th>
+              <th
+                scope="col"
+                className="sticky top-0 z-10 border-b border-gray-300 bg-white/75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+              >
+                {t('model-title')}
+              </th>
+              <th
+                scope="col"
+                className="sticky top-0 z-10 border-b border-gray-300 bg-white/75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+              >
+                {t('movement-type')}
+              </th>
+              <th
+                scope="col"
+                className="sticky top-0 z-10 border-b border-gray-300 bg-white/75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+              >
+                {t('gender')}
+              </th>
+              <th
+                scope="col"
+                className="sticky top-0 z-10 border-b border-gray-300 bg-white/75 py-3.5 pl-3 text-left text-sm font-semibold text-gray-900 pr-4 sm:pr-0 backdrop-blur backdrop-filter"
+              >
+                {t('skeleton')}
+              </th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-gray-200">
+            {models.map(model => (
+              <tr key={model.id}>
+                <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                  {model.reference}
+                </td>
+                <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                  {model.title}
+                </td>
+                <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                  {model.movement_type}
+                </td>
+                <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                  {model.gender}
+                </td>
+                <td className="whitespace-nowrap py-4 pl-3 pr-4 text-sm font-medium text-gray-500 sm:pr-0">
+                  {model.skeleton ? 'yes' : ''}
+                </td>
               </tr>
-            </thead>
-            <tbody className="divide-y divide-gray-200">
-              {models.map(model => (
-                <tr key={model.id}>
-                  <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
-                    {model.reference}
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                    {model.title}
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                    {model.movement_type}
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                    {model.gender}
-                  </td>
-                  <td className="whitespace-nowrap py-4 pl-3 pr-4 text-sm font-medium text-gray-500 sm:pr-0">
-                    {model.skeleton ? 'yes' : ''}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
