d34dab685e802171ba549dd1bf4942585216043a
Ilya Serdyuk <ilya@serdyuk.pro>
2025-06-22 18:29:59 +0300

    Fix обратной миграции

--- migrations/20250509061844_brands.ts
+++ migrations/20250509061844_brands.ts
@@ -8,6 +8,6 @@ export async function up(knex: Knex): Promise<void> {
 
 export async function down(knex: Knex): Promise<void> {
   return knex.raw(
-    'ALTER TABLE `brands` CHANGE `logo_type` `logo_type` enum("svg","png","jpg") NULL COMMENT "Расширение логотипа"',
+    'ALTER TABLE `brands` CHANGE `logo_type` `logo_type` enum("svg","png") NULL COMMENT "Расширение логотипа"',
   );
 }
