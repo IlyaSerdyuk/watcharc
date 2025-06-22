3df40d182cdba395c0d079d58cf7ed7b1664a2f8
Ilya Serdyuk <ilya@serdyuk.pro>
2025-06-22 18:30:41 +0300

    Добавил поддержку логотипов брендов в формате GIF

---
+++ migrations/20250524110433_brands.ts
@@ -0,0 +1,13 @@
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.raw(
+    'ALTER TABLE `brands` CHANGE `logo_type` `logo_type` enum("svg","png","jpg","gif") NULL COMMENT "Расширение логотипа"',
+  );
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.raw(
+    'ALTER TABLE `brands` CHANGE `logo_type` `logo_type` enum("svg","png","jpg") NULL COMMENT "Расширение логотипа"',
+  );
+}
