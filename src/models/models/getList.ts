ba8ff6ee1673891dff84c3741ad40e4e2cec8c49
Ilya Serdyuk <ilya@serdyuk.pro>
2026-01-09 00:16:21 +0300

    Добавил возможность выбирать модели дополнительно ограничивая по коллекции

--- src/models/models/getList.ts
+++ src/models/models/getList.ts
@@ -2,10 +2,14 @@ import { db } from '@services/Db';
 
 import type { ModelType } from './types';
 
-export default async function getList(brandId: number): Promise<ModelType[]> {
-  const models = await db.models
+export default async function getList(brandId: number, collectionId: number = 0): Promise<ModelType[]> {
+  const query = db.models
     .select('*')
     .where('brand_id', '=', brandId)
     .orderBy('reference', 'asc');
+  if (collectionId) {
+    query.andWhere('collection_id', '=', collectionId);
+  }
+  const models = await query;
   return models || [];
 }
